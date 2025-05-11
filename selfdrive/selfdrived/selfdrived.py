e8eea925f32d50c48d1352fc86909ba1672b8529
Mr.one <mawei1991@gmail.com>
2025-05-11 14:54:01 +0800

    Update 2 files
    - /system/hardware/tici/beeper.py
    - /selfdrive/selfdrived/selfdrived.py

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -22,6 +22,17 @@ from openpilot.selfdrive.selfdrived.alertmanager import AlertManager, set_offroa
 from openpilot.selfdrive.controls.lib.latcontrol import MIN_LATERAL_CONTROL_SPEED
 
 from openpilot.system.version import get_build_metadata
+from selfdrive.hardware.tici import beeper
+beeper.setup_beeper()
+engaged_prev = False
+
+# 在状态机循环中添加：
+if CS.enabled and not engaged_prev:
+    beeper.beep_on()
+elif not CS.enabled and engaged_prev:
+    beeper.beep_off()
+
+engaged_prev = CS.enabled
 
 REPLAY = "REPLAY" in os.environ
 SIMULATION = "SIMULATION" in os.environ
