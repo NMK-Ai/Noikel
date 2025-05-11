07505298ffe674ba137e17785ed839e61908a97c
Mr.one <mawei1991@gmail.com>
2025-05-11 17:43:42 +0800

    Update file selfdrived.py

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -277,7 +277,7 @@ class SelfdriveD:
         elif not self.sm.all_freq_ok(self.camera_packets):
           self.events.add(EventName.cameraFrameRate)
     if not REPLAY and self.rk.lagging:
-      self.events.add(EventName.selfdrivedLagging)
+      pass#self.events.add(EventName.selfdrivedLagging)
     if not self.sm.valid['radarState']:
       if self.sm['radarState'].radarErrors.radarUnavailableTemporary:
         self.events.add(EventName.radarTempUnavailable)
