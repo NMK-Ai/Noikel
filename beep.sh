3e7c1ae21b717d9c07f71b4bc7ec3ea0fad7ff36
Mr.one <mawei1991@gmail.com>
2025-05-11 16:02:47 +0800

    Update file beep.sh

--- beep.sh
+++ beep.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
-sudo echo 1 > /sys/class/gpio/gpio42/value
-sudo sleep 0.1
-sudo echo 0 > /sys/class/gpio/gpio42/value
+echo 1 > /sys/class/gpio/gpio42/value
+sleep 0.1
+echo 0 > /sys/class/gpio/gpio42/value
